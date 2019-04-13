e4fa2c62784b161aa39a0a27cae42ef313ed3039
Jacinto <jacintomolina@rocketmail.com>
2019-04-13 14:49:33 -0500

    some changes for ben

--- gui/src/Model/Profile.java
+++ gui/src/Model/Profile.java
@@ -1,5 +1,53 @@
 package Model;
 
+import javafx.scene.image.Image;
+
 public class Profile {
+	
+	private String fistName;
+	private String lastName;
+	private Image getUserImage;
+	private final double maxPercentage = 100;
+	private final double lowPercentage = 0;
+	private double currentPercentage ;
+	
+	
+	public Profile(String fN,String lN, Image im){ // Specific
+			this.fistName = fN;
+			this.lastName = lN;
+			this.getUserImage = im;
+			this.currentPercentage = this.lowPercentage;
+	}
+	
+	public Profile() { // Default
+		
+		this.fistName = "John";
+		this.lastName = "Doe";
+		this.getUserImage = null;
+		this.currentPercentage = this.lowPercentage;
+		
+	}
+	
+	public String getFirstName() {
+		return this.fistName;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public String getFullName() {
+		return this.fistName + " " + this.lastName;
+	}
+	
+	public double getPercentage() {
+		return this.currentPercentage;
+	}
+	
+	public String getPerString() {
+		return this.currentPercentage + "%";
+	}
+	
+	
 
 }
--- gui/src/Model/Scan.java
+++ gui/src/Model/Scan.java
@@ -1,5 +1,25 @@
 package Model;
 
 public class Scan {
+	
+	private static Scan instance;
+	
+	
+	private Scan() {
+		
+	}
+	
+	
+	public static Scan getInstance() {
+		if(instance == null) {
+			instance = new Scan();
+		}
+		return instance;
+	}
 
+
+	// use scanner to read in data.. 
+	
+	
+	
 }
