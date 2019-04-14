fa9e90a3679bb188f293f818dc9bd83b104b7c2d
Jacinto <jacintomolina@rocketmail.com>
2019-04-14 04:53:20 -0500

    ethgfdsafhjuyujuyt

--- gui/src/Model/SystemDeclarations.java
+++ gui/src/Model/SystemDeclarations.java
@@ -2,12 +2,13 @@ package Model;
 
 import java.util.ArrayList;
 
+import javafx.scene.image.Image;
+
 public class SystemDeclarations {
 	
 	public static int currentId = 0;
 	public static ArrayList<Integer> XsOut = new ArrayList<Integer>();
-	
-	
+	public static Image xImage = new Image("/Resource/red-cross.png");
 	public static ArrayList<Integer> getXsList(){
 		return XsOut;
 	}
